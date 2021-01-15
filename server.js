cf31a4ff688814e94d19441e0f98170e3ab64105
motoroboto <robert.a.cullen@gmail.com>
2021-01-15 16:54:52 -0500

    added html routes so all pages can be accessed

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const htmlRoutes = require("./routes/html-routes");
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(require("./routes/html-routes"));
+
 
 
 app.listen(PORT, function () {
