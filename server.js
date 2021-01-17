b94c18ef52c9f78b0b7337ebe6ad00ceba0640be
motoroboto <robert.a.cullen@gmail.com>
2021-01-16 23:14:47 -0500

    more server changes

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const db = require("./models");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+
 app.use(require('./routes/api-routes'));
 app.use(require('./routes/html-routes'));
 
